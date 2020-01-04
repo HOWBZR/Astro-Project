41da450567c46a236b6eebd920814e3fb6e19aee
Christopher Matthews <sirhunty@gmail.com>
2020-01-04 13:24:04 -0700

    Added index.html frame and date range selector .js files

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ $(document).ready(function () {
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
     })
 
 });
+
+
+        $(function () {
+            $('input[name="birthday"]').daterangepicker({
+                singleDatePicker: true,
+                showDropdowns: true,
+                minYear: 1901,
+                maxYear: parseInt(moment().format('YYYY'), 10)
+            }, function (start, end, label) {
+                let formatedDate = $("#date").val()
+                var years = moment(formatedDate).format("YYYY-MM-DD");
+                console.log(years);
+                // alert("You are " + years + " years old!");
+            });
+        });
